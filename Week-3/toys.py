e2c922c84466a1e607fe946f2223c95b482db957
elysemarshall <elyse.marshall19@imperial.ac.uk>
2019-10-22 16:48:36 +0100

    toys.practicingpushing

--- Week-3/toys.py
+++ Week-3/toys.py
@@ -8,21 +8,23 @@ with functions.
 
 # write a function that adds 1
 # to the input and prints the result
-def inc(a):
+def inc(a): 
+    a = a + 1
     print(a)
 
 
 # write a function that adds 1
 # to the input and returns the result
 def inc_return(a):
-    return # hint this is incomplete
+    return a + 1  # hint this is incomplete
 
 
 # write a function that adds
 # the two input numbers together
 # and returns the sum
 def sum(a, b):
-    return
+    sum_ab = a + b 
+    return sum_ab
 
 
 # write a function that takes two
@@ -30,14 +32,17 @@ def sum(a, b):
 # sum() and then increments the sum
 # using inc_return
 def sum_inc(a, b):
-    return
+    sum2 = sum(a, b)
+    sum2 = inc_return(sum2)
+    return sum2
 
 
 # write a function that returns a 
 # boolean (True or False) for whether 
 # the input number is even
 def is_even(a):
-    return
+    even = a % 2 == 0
+    return even
 
 
 # create for loop that takes a string
@@ -47,7 +52,13 @@ def is_even(a):
 # e.g. string_repeat('ho', 3) returns
 # 'hohoho'
 def string_repeat(phrase, repeat):
+
+    s = str()
+    for sentance in range(repeat):
+     s += phrase
+    print(s)
     # hint: you can add strings together 
     # in order to concatenate them
-    return
+
+string_repeat('ha', 3)
 
