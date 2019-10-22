7fcfe0a002efdba82fc8cca23aae94b22cd9041a
elysemarshall <elyse.marshall19@imperial.ac.uk>
2019-10-22 17:45:54 +0100

    First edit

--- Week-4/structures.py
+++ Week-4/structures.py
@@ -9,8 +9,9 @@ Simple functions performing operations on basic Python data structures.
 # write a function that returns a list containig the first and the last element
 # of "the_list". 
 def first_and_last(the_list):
-    return []
-
+    the_list = ['apple', 'banana', 'pear', 'orange']
+    return (the_list[0,-1])
+    
 
 # write a function that returns part of "the_list" between indices given by the
 # second and third parameter, respectively. The returned part should be in
